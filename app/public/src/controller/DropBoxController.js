91a1d9b109af8ca749b860197311537ad7bed73d
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-07-13 19:37:38 -0300

    Selecionando arquivos com CTRL ou SHIFT

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -401,7 +401,7 @@ class DropBoxController {
                 let key = snapshotItem.key
                 let data = snapshotItem.val()
 
-                this.listFilesEl.appendChild(this.getFileView(data, key))
+                this.listFilesEl.appendChild(this.getFileView(data, key)) 
             })
         })
     }
@@ -409,6 +409,40 @@ class DropBoxController {
     initEventsLi(li) {
 
         li.addEventListener('click', e => {
+
+            if (e.shiftKey) {
+                let firstLi = this.listFilesEl.querySelector('.selected')
+
+                if (firstLi) {
+                    let indexStart
+                    let indexEnd
+                    let lis = li.parentElement.childNodes
+
+                    lis.forEach((el, index) => {
+                        if (firstLi === el) indexStart = index
+                        if (li === el) indexEnd = index
+                    })
+
+                    let index = [indexStart, indexEnd].sort()
+
+                    lis.forEach((el, i) => {
+
+                        if (i >= index[0] && i <= index[1]) {
+                            el.classList.add('selected')
+                        }
+                    })
+
+                    return true
+                }
+
+            }
+
+            if (!e.ctrlKey) { 
+                this.listFilesEl.querySelectorAll('li.selected').forEach(el => {
+                    el.classList.remove('selected')
+                })
+            }
+
             li.classList.toggle('selected')
         })
     }
